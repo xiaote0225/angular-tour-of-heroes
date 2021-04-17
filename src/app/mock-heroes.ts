495506c234038cefd7d5f4ec2a17037a6fa0ba20
wangxun <wangxun0225@163.com>
2021-04-17 14:50:19 +0800

    feat:显示英雄列表;

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'name001' },
+  { id: 12, name: 'name002' },
+  { id: 13, name: 'name003' },
+  { id: 14, name: 'name004' },
+  { id: 15, name: 'name005' },
+  { id: 16, name: 'name006' },
+  { id: 17, name: 'name007' },
+  { id: 18, name: 'name008' },
+  { id: 19, name: 'name009' },
+  { id: 20, name: 'name010' }
+];
