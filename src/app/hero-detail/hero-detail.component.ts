127d419abd634408c98e1568f510fae300ff380c
wangxun <wangxun@cyai.com>
2021-04-20 13:55:10 +0800

    feat:添加应用内导航;

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -8,18 +8,18 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
-
   // @Input() hero: Hero | undefined;
+  // hero: Hero | undefined;
   hero: Hero | undefined;
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
@@ -31,12 +31,14 @@ export class HeroDetailComponent implements OnInit {
   // }
 
   getHero(): void {
-    // const id = +this.route.snapshot.paramMap.get('id');
-    const id = 12;
-    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+    const id = this.route.snapshot.paramMap.get('id');
+    // const id = 12;
+    this.heroService
+      .getHero(Number(id))
+      .subscribe((hero) => (this.hero = hero));
   }
 
-  goBack():void {
+  goBack(): void {
     this.location.back();
   }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,19 +6,18 @@ import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+  constructor(private messageService: MessageService) {}
 
-  constructor(private messageService: MessageService) { }
-
-  getHeroes():Observable<Hero[]> {
+  getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
   getHero(id: number): Observable<Hero | undefined> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id ));
+    return of(HEROES.find((hero) => hero.id === id));
   }
 }
